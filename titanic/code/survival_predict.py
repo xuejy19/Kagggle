9d69d532c3924f589af2e2cbf24b86d0f5a3844c
xuejianye <xuejy19@mails.tsinghua.edu.cn>
2020-08-30 09:42:49 +0800

    code

---
+++ titanic/code/survival_predict.py
@@ -0,0 +1,9 @@
+'''
+Description: titanic_survival_predict
+Version: V1.0
+Author: xuejy19@mails.tsinghua.edu.cn
+Date: 2020-08-30 09:41:49
+'''
+import numpy as np 
+import pandas as pd 
+import os 
